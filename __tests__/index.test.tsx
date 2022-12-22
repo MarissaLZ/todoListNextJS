089595c27a06007b87423fd42c5f1b38292969c5
Marissa Lazo <marissalazo@Marissas-MacBook-Pro.local>
2022-12-21 22:48:04 -0500

    correction to component test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -10,9 +10,9 @@ import TodoList from "../components/TodoList"
 describe("AddForm", () => {
   test("Component renders and passes props", () => {
     const addList = jest.fn()
-  //   render(<AddForm label="Add a List" path="lists" requestType={addList} />)
-  //   expect(screen.getByText("Add a List")).toBeInTheDocument()
-  // })
+    //   render(<AddForm label="Add a List" path="lists" requestType={addList} />)
+    //   expect(screen.getByText("Add a List")).toBeInTheDocument()
+  })
   test("CurrentLists Component passes props", () => {
     const names = [
       { _id: "63879975b7986744f0415790", name: "list a", __v: 0 },
